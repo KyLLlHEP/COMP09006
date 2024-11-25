852a1466fcce2985d9737cf4e0108439ada41ece
Artur Kushner <kushnerartur@gmail.com>
2024-11-25 11:46:27 +0000

    feat: button active

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector('.hero-button').addEventListener('click', function () {
+    window.location.href = 'gifts.html';
+});
+document.querySelector('.cta-button').addEventListener('click', function () {
+    window.location.href = 'gifts.html';
+});
